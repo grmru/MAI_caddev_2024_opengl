e3d95dcd7927bdc5cedf668e2d45a61407cee25c
Георгий Цырков <g@tias.pro>
2024-03-11 22:13:09 +0300

    ...

--- Program.cs
+++ Program.cs
@@ -7,12 +7,11 @@ public static class Program
     public static int Main()
     {
         Core core = new Core(800, 600, "MAI Разработка САПР");
-        
+
         core.Run();
 
-        core.Close();
-        core.Dispose();
         Console.WriteLine("Done");
+        
         return 0;
     }
 }
